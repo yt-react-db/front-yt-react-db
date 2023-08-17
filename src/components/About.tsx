d39f590fc61e7ea0b4d2962f6468a6c8f6f565c6
ComputerBread <contact.computerbread@gmail.com>
2023-08-17 15:46:20 +0200

    about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,6 @@
+import { Github, Twitter, Youtube } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function About() {
     return (
         <div>
@@ -6,14 +9,69 @@ export default function About() {
                 <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">About</h2>
             </div>
             <p>
-                TODO:
-                - Fairuse is a spectrum of transformation (+ documentary, commentary, reaction -)
-                - React content is a the limit between fair and unfair use
-                - (potential) Pros of react: discoverability, algorithm boost, relevance
-                - (potential) Cons of react: views? monetization
-                -
-            </p>
+                <Link className="hover:underline" to="https://github.com/orgs/yt-react-db/repositories">Open Source Project</Link>. Not affiliated with YouTube.<br /><br />
+                React content on YouTube is controversial. It sparks arguments
+                and debates between original content creators and reactors.
+                Each reaction is distinct.
+                The most transformative ones, such as documentaries and in-depth
+                analyses, are considered to fall under fair use. On the other
+                hand, reactions that are less transformative, like commentaries
+                and simpler reactions, are subject to scrutiny regarding
+                whether they qualify as fair use or not.<br />
 
+                When the question can be asked, whether or not a reaction is
+                transformative enough, it is better to ask for consent.<br />
+
+                <span className="font-bold underline">yt-react-db</span> aims to
+                collect the permissions from youtubers to let reactors know if
+                they can watch their videos live and upload their reaction on youtube.<br />
+                <br />
+                <span className="font-bold underline">yt-react-db</span> also wants
+                to provide tools (like an extension) to help reactors & editors
+                know if they can react and upload their reactions.<br />
+                <br />
+                A potential future goal of <span className="font-bold underline">yt-react-db</span> could
+                be to collect data and produce statistical analyses to demonstrate
+                the impact of react content on original content.<br />
+                The lack of such data, can make it difficult to decide if react
+                has a positive (discoverability? algorithm boost? free exposure? ...)
+                or negative impact (cannibalism? monetization? ...).
+
+            </p>
+            <div id="contact" className="my-8">
+                <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Contact</h2>
+                This project is developed and maintained by <Link className="underline hover:text-blue-500" to="https://twitter.com/ComputerBread">@ComputerBread</Link>.<br />
+                Please use <Link className="underline hover:text-blue-500" to="https://twitter.com/ComputerBread">twitter</Link> to contact me.
+                Do not use <Link to="https://twitter.com/yt_react_db" className="line-through">@yt_react_db</Link> (I am too lazy to manage several twitter accounts).<br />
+                <br />
+                You can also find me on:
+                <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
+                    <li className="flex items-center"><Twitter /><p>X (Twitter) as <Link className="underline hover:text-blue-500" to="https://twitter.com/ComputerBread">@ComputerBread</Link> (preferred)</p></li>
+                    <li className="flex items-center"><Youtube /><p>Youtube as <Link className="underline hover:text-red-500" to="https://www.youtube.com/ComputerBread">@ComputerBread</Link> or <Link className="hover:text-red-500 underline" to="https://www.youtube.com/@computerbreadboard">@ComputerBreadBoard</Link></p></li>
+                    <li className="flex items-center"><Github />Github&nbsp;<Link to="https://github.com/ComputerBread" className="underline hover:text-gray-500 underline-offset:2">@ComputerBread</Link></li>
+                </ul>
+
+            </div>
+            <div id="discuss_participate" className="my-8">
+                <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Discuss & Participate</h2>
+                <p>
+                    This project is open source.
+                    For discussions, please use <Link className="underline hover:text-blue-500" to="https://twitter.com/ComputerBread">Twitter (X)</Link> or
+                    &nbsp;<Link to="https://github.com/yt-react-db/issue-tracker/discussions" className="underline hover:text-gray-500">Github Discussions</Link>.<br />
+                    Reddit & Discord will come next if needed (don't have time or attention to set and moderate them now!).
+                </p>
+                <p><Link to="https://github.com/yt-react-db/issue-tracker/issues" className="underline hover:text-gray-500">For anything else, please create a new issue!</Link></p>
+            </div>
+
+            <div id="discuss_participate" className="my-8">
+
+                <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Support the project</h2>
+                <p>If you want to support the project, <Link to="https://ko-fi.com/computerbread" className="underline hover:text-pink-500">please consider making a donation</Link></p>
+            </div>
+
+            <p>
+
+            </p>
             <div className="my-8">
 
                 <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Roadmap:</h2>
@@ -69,9 +127,6 @@ export default function About() {
 
             </div >
 
-            <div id="contact" className="my-8">
-                <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Contact</h2>
-            </div>
 
         </div >
     )
