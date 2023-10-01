b064cf63408b9294a49c4b65210d2554da9bf2c3
ComputerBread <contact.computerbread@gmail.com>
2023-10-01 18:45:43 +0200

    title + favicon + remove useless img

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,6 @@ export default function Header() {
     return (
         <header>
             <div className="flex flex-col justify-content items-center mt-10">
-                <div hidden>
-                    <img
-                        src="public/logo.png"
-                        alt="Header Image"
-                        className="rounded-full block w-72 my-10"
-                    />
-                </div>
                 <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-yellow-500">Youtube React Database</span></h1>
                 <p className="text-lg font-normal text-gray-900 lg:text-l dark:text-gray-400 text-center">
                     Collecting permissions from YouTubers to react to their content live and/or upload the reactions on YouTube!
