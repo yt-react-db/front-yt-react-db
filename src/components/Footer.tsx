21e1871fdad6ecb0df24d3ef33d187b755b84789
ComputerBread <contact.computerbread@gmail.com>
2023-10-12 23:59:59 +0200

    put my username in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className="my-4">
             <Separator />
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://yt-react-db.com/" className="hover:underline">yt-react-db</a>. Not affiliated with YouTube.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://yt-react-db.com/" className="hover:underline">yt-react-db</a> by <a href="https://twitter.com/ComputerBread" className="text-amber-600 hover:underline">ComputerBread</a>. Not affiliated with YouTube.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
