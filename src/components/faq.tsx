b22e427cd56205dfd2bb934fde47ceaf3d903120
ComputerBread <contact.computerbread@gmail.com>
2023-10-13 02:14:57 +0200

    add note on how to modify permissions

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -15,12 +15,20 @@ export default function FAQ() {
                 </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-                <AccordionTrigger>Do you collect personal data?</AccordionTrigger>
+                <AccordionTrigger>Do you collect sensitive personal data?</AccordionTrigger>
                 <AccordionContent>
                     No.<br /><br />
                     We only collect your YouTube channel's name & ID, which are public information anyone can find. <br />
                     Our server never sees your email address or any other personal information.<br /><br />
                     Don't believe it? <a className="underline hover:text-gray-500" href="https://github.com/orgs/yt-react-db/repositories">Check the source code on GitHub</a>
+                </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+                <AccordionTrigger>How to modify my permissions?</AccordionTrigger>
+                <AccordionContent>
+                    If you already gave us your permissions, you can go through the same process below to modify them.
+                    Note that we keep an history of your previous permissions, so you can't falsely accuse someone to not respect your choices.
+                    Note that it can take up to 24 hours for extension users to see the changes!
                 </AccordionContent>
             </AccordionItem>
         </Accordion>
